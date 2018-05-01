48296cfc355529a5b587f3b8efa4cb895a381836
valoulou <valentin.chaigneau@gmail.com>
2018-05-01 19:42:34 -0400

    Community Expandable List

--- app/src/main/java/jajcompany/jajmeup/Fragment/CommunityFragment.kt
+++ app/src/main/java/jajcompany/jajmeup/Fragment/CommunityFragment.kt
@@ -5,10 +5,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import jajcompany.jajmeup.R
+import jajcompany.jajmeup.Utils.CommunityExpandableAdapter
+import kotlinx.android.synthetic.main.community_layout.*
 
 class CommunityFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.community_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listFriend = mutableListOf<CommunityExpandableAdapter.itemCommunauty>()
+        val tmp: CommunityExpandableAdapter.itemCommunauty = CommunityExpandableAdapter.itemCommunauty("BOB", R.drawable.abc_ic_star_black_36dp)
+
+        listFriend.add(tmp)
+
+        val listWorld = mutableListOf<CommunityExpandableAdapter.itemCommunauty>()
+        val tmp2: CommunityExpandableAdapter.itemCommunauty = CommunityExpandableAdapter.itemCommunauty("ALICE", R.drawable.abc_ic_star_half_black_36dp)
+
+        listWorld.add(tmp2)
+
+        val listHeader = listOf("Mes amis", "Tout le monde")
+        val listChild = HashMap<String, MutableList<CommunityExpandableAdapter.itemCommunauty>>()
+
+        listChild.put(listHeader[0], listFriend)
+        listChild.put(listHeader[1], listWorld)
+
+        val expandableAdapter = CommunityExpandableAdapter(activity, listHeader, listChild)
+
+        community_list.setAdapter(expandableAdapter)
     }
 }
---
+++ app/src/main/java/jajcompany/jajmeup/Utils/CommunityExpandableAdapter.kt
@@ -0,0 +1,72 @@
+package jajcompany.jajmeup.Utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import jajcompany.jajmeup.R
+
+class CommunityExpandableAdapter(val context: Context, val listHeader: List<String>, val listChild: HashMap<String, MutableList<itemCommunauty>>) : BaseExpandableListAdapter() {
+    override fun getGroup(groupPosition: Int): Any {
+        return listHeader[groupPosition]
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+        return true
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
+        val headerValue = getGroup(groupPosition) as String
+        val view: View = LayoutInflater.from(context).inflate(R.layout.community_list_header, parent, false)
+        val listHeaderTxt = view.findViewById<TextView>(R.id.header_communauty) as TextView
+
+        listHeaderTxt.setTypeface(null, Typeface.BOLD)
+        listHeaderTxt.text = headerValue
+
+        return view
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return listChild[listHeader[groupPosition]]!!.size
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        return listChild[listHeader[groupPosition]]!![childPosition]
+    }
+
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
+    }
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
+        val child = getChild(groupPosition, childPosition) as CommunityExpandableAdapter.itemCommunauty
+        val view: View = LayoutInflater.from(context).inflate(R.layout.community_list_item, parent, false)
+        val listItemTxt = view.findViewById<TextView>(R.id.userNameList)
+        val listItemImg = view.findViewById<ImageView>(R.id.userPicturesList)
+
+        listItemImg.setImageResource(child.profilPicture)
+        listItemTxt.text = child.userName
+
+
+        return view
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun getGroupCount(): Int {
+        return listHeader.size
+    }
+
+    data class itemCommunauty(val userName: String, val profilPicture: Int)
+}
